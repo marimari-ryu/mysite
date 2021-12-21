0e324906832c561478776f2b1a829112a35d595c
marimari-ryu <acid-ryuu-0423@ezweb.ne.jp>
2021-12-21 15:23:26 +0900

    third commit

--- src/controller/LoginServlet.java
+++ src/controller/LoginServlet.java
@@ -46,7 +46,7 @@ public class LoginServlet extends HttpServlet {
 			DBManager dbm = new DBManager();
 		//	UserDTO user = dbm.getLoginUser(loginId, pass);
 
-
+				//aaa
 			if (user != null) {
 				// ユーザ情報を取得できたら、書き込み内容リストを取得
 				//ArrayList<ShoutDTO> list = dbm.getShoutList();
